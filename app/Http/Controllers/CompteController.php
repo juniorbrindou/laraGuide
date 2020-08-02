d467f8a9b0628ac2dd83b75ff30e3641a7092d92
225junior <225juniorbrindou@gmail.com>
2020-08-02 12:45:46 +0000

    processus de mise a jour du mot de passe

--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class CompteController extends Controller
@@ -21,7 +21,29 @@ class CompteController extends Controller
     public function logout(){
 
     	Auth()->logout();
+    	
     	flash('Vous etes déconnecté avec Succes')->success();
+
     	return redirect('/');
+    }
+
+    public function password_update(){
+
+    	if (Auth::check()) {
+
+    		Request()->validate([
+    			'password'=>['required','min:4','confirmed'],
+    			'password_confirmation'=>['required']
+    		]);
+
+    		Auth::user()->update(['password'=>bcrypt(request()->password)]);
+
+    		flash('Mot de passe Modiffié avec success!')->success();
+
+    		return back();
+
+    	}else{
+    		return redirect('login');
+    	}
     }
 }
