5863d9b1043cac7854128724e8ebd8b36ed41bbd
225junior <225juniorbrindou@gmail.com>
2020-08-22 15:26:18 +0000

    form add avatar

--- resources/views/mon-compte.blade.php
+++ resources/views/mon-compte.blade.php
@@ -7,7 +7,28 @@
 
 </div>
 
+<!-- formulaire de photo de profile -->
+<form method="POST" action="avatar_update" class="section">
+	@csrf
+	<div class="columns">
+		<div class="field column">
+			<label class="label">Nouvel Avatar</label>
+				<input class="input" type="file" name="avatar" placeholder="avatar">
+				@if($errors->has('avatar'))
+					<span class="help is-danger">{{$errors->first('avatar')}}</span>
+				@endif
+		</div>
+	</div>
 
+	<div class="field">
+		<div class="control">
+			<input type="submit" class="button is-link" name="submit" value="Modiffier Mot de Passe">
+		</div>
+	</div>
+</form>
+
+
+<!-- formulaire de mot de passe -->
 <form method="POST" action="password_update" class="section">
 	@csrf
 	<div class="columns">
